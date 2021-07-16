61c8346ae7b332ed681494bcf2cb9fc67ae30e95
Dakouri M. Kobri <65511636+DakouriKobri@users.noreply.github.com>
2021-07-16 11:53:56 +0200

    Revert "1. Change "Shopping List" tag to h1"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import ShoppingItem from "./components/ShoppingItem";
-
 function App() {
-  return (
-    <div>
-      <div>
-        <h1>Shopping List</h1>
-      </div>
-      <ShoppingItem />
-    </div>
-  );
+  return <div>Shopping List</div> 
 }
 
 export default App;
