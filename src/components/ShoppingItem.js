8983e803d74c8efca6ed1a70f2e2fc033f4f8d36
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-07-16 11:39:45 +0200

    1. Change "Shopping List" tag to h1 2. Create ShoppingItem component 3. Add checkbox, item name and price to ShoppingItem

---
+++ src/components/ShoppingItem.js
@@ -0,0 +1,15 @@
+function ShoppingItem() {
+  return (
+    
+      <li>
+        <input type="checkbox" />
+        <div>
+          <p>Name</p>
+          <p>Price</p>
+        </div>
+      </li>
+    
+  );
+}
+
+export default ShoppingItem;
